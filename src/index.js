94daaf9b8c931ce27c44308633501ee8d5b372ac
AbdoElfathi <abdessamadelfathi2018@gmail.com>
2024-01-12 18:17:13 +0100

    simple changes

--- src/index.js
+++ src/index.js
@@ -49,7 +49,21 @@ const pizzaData = [
 function App() {
   return (
     <div>
-      <h1>Hello React!</h1>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>Fast React Pizza Co.</h1>;
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Our menu</h2>
       <Pizza />
       <Pizza />
       <Pizza />
@@ -60,15 +74,17 @@ function App() {
 function Pizza() {
   return (
     <div>
-      <img
-        src="pizzas/spinaci.jpg"
-        alt="Pizza Spinaci"
-        width="300"
-        height="300"
-      />
+      <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci" />
       <h2>Pizza Spinaci</h2>
       <p>Tomato, mozarella, spinach, and ricotta cheese</p>
     </div>
+  );
+}
+
+function Footer() {
+  // return React.createElement("footer", null, "We're currently open");
+  return (
+    <footer>{new Date().toLocaleTimeString()} We're currently open</footer>
   );
 }
 
